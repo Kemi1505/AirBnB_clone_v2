becff938c20c65a549048a5c0b8e0885ec77dce7
Kemi1505 <oluwakemikuteyi1505@gmail.com>
2024-07-28 17:28:57 -0700

    airbnb

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""starts the application"""
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index():
+    """displays phrase"""
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """displays phrase"""
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
